707d8be17d5854cc336e1b35e75d02e5a759db11
sidchang <sidchang@gmail.com>
2023-02-27 13:31:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner") 
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Egg White Omelette")
-streamlit.text("Steel Cut Oatmeal")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
